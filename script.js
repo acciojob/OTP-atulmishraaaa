3fc598b871bc000bd9394411c9dadba69f27fa57
Atul Mishra <139470533+atulmishraaaa@users.noreply.github.com>
2024-02-21 14:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.getElementById("digits1").disabled = true;
+document.getElementById("digits2").disabled = true;
+document.getElementById("digits3").disabled = true;
+document.getElementById("digits4").disabled = true;
+document.getElementById("digits5").disabled = true;
+document.getElementById("digits6").disabled = true;
